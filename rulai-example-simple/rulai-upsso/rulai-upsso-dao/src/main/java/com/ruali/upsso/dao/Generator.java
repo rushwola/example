96dcd78adf49ea2dce8c98dcb450629446740ffa
rushwola <1474920845@qq.com>
2017-06-12 23:09:34 +0800

    新建了单点登录和用户权限管理项目

---
+++ rulai-example-simple/rulai-upsso/rulai-upsso-dao/src/main/java/com/ruali/upsso/dao/Generator.java
@@ -0,0 +1,21 @@
+package com.ruali.upsso.dao;
+
+/**
+ * @ClassName Generator
+ * @Description TODO(生成代码类)
+ * @author ly
+ * @Date 2017年6月12日 下午11:03:31
+ * @version 1.0.0
+ */
+public class Generator {
+
+	/**
+	 * 自动代码生成
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+
+	}
+
+}
