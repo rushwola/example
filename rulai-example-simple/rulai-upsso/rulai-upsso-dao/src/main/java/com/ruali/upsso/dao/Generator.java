2f530d1d8521be4756190aad23eed5c1429d8e59
rushwola <1474920845@qq.com>
2017-06-13 23:41:07 +0800

    upsso模块添加了dao代码，新建　了一些框架的东西

--- rulai-example-simple/rulai-upsso/rulai-upsso-dao/src/main/java/com/ruali/upsso/dao/Generator.java
+++ rulai-example-simple/rulai-upsso/rulai-upsso-dao/src/main/java/com/ruali/upsso/dao/Generator.java
@@ -1,5 +1,11 @@
 package com.ruali.upsso.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.rulai.tool.velocity.MybatisGeneratorUtil;
+import com.rulai.tool.velocity.entry.MybatisConfigParam;
+
 /**
  * @ClassName Generator
  * @Description TODO(生成代码类)
@@ -8,6 +14,29 @@ package com.ruali.upsso.dao;
  * @version 1.0.0
  */
 public class Generator {
+	
+private static final String  DATABASE="zheng";
+	
+	private static final String JDBC_DRIVER="com.mysql.jdbc.Driver";
+	
+	private static final String JDBC_PASSWORD="qding";
+	
+	private static final String JDBC_URL="jdbc:mysql://120.24.181.119:3307/zheng?useUnicode=true&characterEncoding=utf-8&autoReconnect=true";
+
+	private static final String JDBC_USERNAME="qding";
+	
+	private static final String TABLE_PREFIX = "upms_";
+	
+	private static final String PACKAGE_NAME = "com.rulai.tool.velocity";
+	
+	private static final Map<String, String> LAST_INSERT_ID_TABLES = new HashMap<>();
+	
+	private static final String TARGET_PROJECT = "F:/example-git/rulai-example-simple/rulai-upsso/rulai-upsso-dao";//velocity-tool
+	
+	private static final String TARGET_PROJECT_SQLMAP = "F:/example-git/rulai-example-simple/rulai-upsso/rulai-upsso-dao";
+	static {
+		LAST_INSERT_ID_TABLES.put("upms_user", "user_id");
+	}
 
 	/**
 	 * 自动代码生成
@@ -15,7 +44,20 @@ public class Generator {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-
+		MybatisConfigParam param=new MybatisConfigParam();
+		param.setDatabase(DATABASE);
+		param.setJdbc_driver(JDBC_DRIVER);
+		param.setJdbc_password(JDBC_PASSWORD);
+		param.setJdbc_url(JDBC_URL);
+		param.setJdbc_username(JDBC_USERNAME);
+		param.setLast_insert_id_tables(LAST_INSERT_ID_TABLES);
+		param.setPackage_name(PACKAGE_NAME);
+		param.setTargetProject(TARGET_PROJECT);
+		param.setTable_prefix(TABLE_PREFIX);
+		param.setTargetProject_sqlMap(TARGET_PROJECT_SQLMAP);
+		
+		
+		MybatisGeneratorUtil.mybatisGenerator(param);
 	}
 
 }
