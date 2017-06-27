f9026f7a002c6ee2787d703152dda10385725307
rushwola <1474920845@qq.com>
2017-06-27 23:20:04 +0800

    添加了单点登录client代码

---
+++ rulai-framework-simple/rulai-framework-sso/rulai-framework-sso-client/src/main/java/com/rulai/framework/sso/client/session/SingleSignOutFilter.java
@@ -0,0 +1,73 @@
+package com.rulai.framework.sso.client.session;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.rulai.framework.sso.client.util.AbstractConfigurationFilter;
+import com.rulai.framework.sso.client.util.CommonUtils;
+import com.rulai.framework.sso.client.util.XmlUtils;
+
+/**
+ * Implements the Single Sign Out protocol.  It handles registering the session and destroying the session.
+ *
+ * @author Scott Battaglia
+ * @version $Revision$ $Date$
+ * @since 3.1
+ */
+public final class SingleSignOutFilter extends AbstractConfigurationFilter {
+
+    /**
+     * The name of the artifact parameter.  This is used to capture the session identifier.
+     */
+    private String artifactParameterName = "ticket";
+
+    public void init(final FilterConfig filterConfig) throws ServletException {
+        setArtifactParameterName(getPropertyFromInitParams(filterConfig, "artifactParameterName", "ticket"));
+
+        init();
+    }
+
+    public void init() {
+        CommonUtils.assertNotNull(this.artifactParameterName, "artifactParameterName cannot be null.");
+    }
+
+    public void setArtifactParameterName(final String artifactParameterName) {
+        this.artifactParameterName = artifactParameterName;
+    }
+
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        if ("POST".equals(request.getMethod())) {
+            final String logoutRequest = request.getParameter("logoutRequest");
+
+            if (CommonUtils.isNotBlank(logoutRequest)) {
+                final String sessionIdentifier = XmlUtils.getTextForElement(logoutRequest, "SessionIndex");
+
+                if (CommonUtils.isNotBlank(sessionIdentifier)) {
+                    SingleSignOutHttpSessionListener.removeSession(sessionIdentifier);
+                    return;
+                }
+            }
+        } else {
+            final String artifact = request.getParameter(this.artifactParameterName);
+            final HttpSession session = request.getSession();
+            if (CommonUtils.isNotBlank(artifact)) {
+                SingleSignOutHttpSessionListener.addSession(artifact, session);
+            }
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void destroy() {
+        // nothing to do
+    }
+}
---
+++ rulai-framework-simple/rulai-framework-sso/rulai-framework-sso-client/src/main/java/com/rulai/framework/sso/client/session/SingleSignOutHttpSessionListener.java
@@ -0,0 +1,69 @@
+package com.rulai.framework.sso.client.session;
+
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Listener to detect when an HTTP session is destroyed and remove it from the map of
+ * managed sessions.  Also allows for the programmatic removal of sessions.
+ * <p>
+ * Enables the CAS Single Sign out feature.
+ * <p>
+ * Note that this class does not scale to multiple machines. 
+ *
+ * Scott Battaglia
+ * @version $Revision$ Date$
+ * @since 3.1
+ */
+public final class SingleSignOutHttpSessionListener implements HttpSessionListener {
+
+    /**
+     * Maps the ID from the CAS server to the Session.
+     */
+    private static final Map MANAGED_SESSIONS = new HashMap();
+
+    /**
+     * Maps the Session ID to the key from the CAS Server.
+     */
+    private static final Map ID_TO_SESSION_KEY_MAPPING = new HashMap();
+
+    public void sessionCreated(final HttpSessionEvent event) {
+        // nothing to do at the moment
+    }
+
+    public void sessionDestroyed(final HttpSessionEvent event) {
+        final HttpSession session = event.getSession();
+        final String key = (String) ID_TO_SESSION_KEY_MAPPING.get(session.getId());
+        MANAGED_SESSIONS.remove(key);
+        ID_TO_SESSION_KEY_MAPPING.remove(session.getId());
+    }
+
+    public static void addSession(final String key, final HttpSession value) {
+        ID_TO_SESSION_KEY_MAPPING.put(value.getId(), key);
+        MANAGED_SESSIONS.put(key, value);
+    }
+
+    /**
+     * Method to remove the session from the mapping based on the key returned from the
+     * CAS server.
+     *
+     * @param key the key to look up in the map of sessions.
+     */
+    public static void removeSession(final String key) {
+        final HttpSession session = (HttpSession) MANAGED_SESSIONS.get(key);
+
+        if (session == null) {
+            return;
+        }
+
+        final String id = session.getId();
+        MANAGED_SESSIONS.remove(key);
+        ID_TO_SESSION_KEY_MAPPING.remove(id);
+
+        session.invalidate();
+    }
+}
