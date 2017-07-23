66681567e71945fa108bb344c492d52a26a82c37
rushwola <1474920845@qq.com>
2017-07-23 23:13:44 +0800

    添加了一个测试类

---
+++ rulai-example-simple/rulai-upsso/rulai-upsso-service/src/test/java/com/rulai/example/upsso/UpssoUserServiceImplTest.java
@@ -0,0 +1,124 @@
+package com.rulai.example.upsso;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UpssoUserServiceImplTest {
+
+	@Test
+	public void testCountByExample() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteByExample() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteByPrimaryKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertSelective() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectByExampleWithBLOBs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectByExample() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectByExampleWithBLOBsForStartPage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectByExampleForStartPage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectByExampleWithBLOBsForOffsetPage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectByExampleForOffsetPage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectFirstByExample() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectFirstByExampleWithBLOBs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectByPrimaryKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateByExampleSelective() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateByExampleWithBLOBs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateByExample() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateByPrimaryKeySelective() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateByPrimaryKeyWithBLOBs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateByPrimaryKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteByPrimaryKeys() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitMapper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMapperClass() {
+		fail("Not yet implemented");
+	}
+
+}
